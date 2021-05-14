30881c3a5dbe5c328c6a9e6727dc05ef796a4395
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-14 15:04:53 -0300

    feat: creating UseEffectComponent

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { UseEffectComponent } from './src/components/UseEffectComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <UseEffectComponent />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/UseEffectComponent.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Text } from 'react-native';
+
+const LOG = '[USE EFFECT COMPONENT]';
+
+export const UseEffectComponent = () => {
+  const [isHappy, setIsHappy] = useState(false);
+
+  useEffect(() => {
+    console.log(LOG, 'mounted');
+  }, []);
+
+  useEffect(() => {
+    console.log(LOG, 'updated');
+  });
+
+  useEffect(() => {
+    console.log(LOG, 'isHappy updated');
+  }, [isHappy]);
+
+  return (
+    <>
+      <Text>Am I Happy? {isHappy ? 'YES' : 'NO'}</Text>
+      <Button title="Change Happiness" onPress={() => setIsHappy(!isHappy)}/>
+    </>
+  )
+};
