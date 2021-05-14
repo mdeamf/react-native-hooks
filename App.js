c731b441819d8c797e09a6c489fd1cbbebc84dff
mdeamf <mauriciodealmeidamf@gmail.com>
2021-05-14 15:39:29 -0300

    feat: creating UseEffectCleaningComponent

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { UseEffectCleaningComponent } from './src/components/UseEffectCleaningComponent';
 import { UseEffectComponent } from './src/components/UseEffectComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <UseEffectComponent />
+
+      <UseEffectCleaningComponent />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/UseEffectCleaningComponent.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Text } from 'react-native';
+
+const LOG = '[USE EFFECT COMPONENT]';
+
+export const UseEffectCleaningComponent = () => {
+  const [isSad, setIsSad] = useState(0);
+
+  const getSadness = () => {
+    switch (isSad) {
+      case 0:
+        return 'NO';
+      case 1:
+        return 'BECOMING SAD...';
+      case 2:
+        return 'YES';
+    }
+  };
+
+  useEffect(() => {
+    console.log(LOG, 'mounted');
+  }, []);
+
+  useEffect(() => {
+    console.log(LOG, 'updated');
+  });
+
+  useEffect(() => {
+    console.log(LOG, 'isSad updated', isSad);
+
+    // If it's becoming sad, dispatch an interval and clean it afterwards.
+    if (isSad === 1) {
+      const interval = setInterval(() => {
+        console.log(LOG, 'isSad finished updating', isSad);
+        setIsSad(2);
+      }, 1500);
+
+      return () => clearInterval(interval);
+    }
+  }, [isSad]);
+
+  return (
+    <>
+      <Text>Am I Sad? {getSadness()}</Text>
+      <Button
+        title="Change Sadness"
+        onPress={() => setIsSad(isSad === 0 ? 1 : 0)}
+        disabled={isSad === 1}
+      />
+    </>
+  );
+};
